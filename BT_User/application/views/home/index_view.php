99fe1d32bb32de219f5f476c0bbda88c3e47c317
jimin123 <nguyenduyminh0505@gmail.com>
2016-10-01 20:55:30 +0700

    This is file example Login and Logup

---
+++ BT_User/application/views/home/index_view.php
@@ -0,0 +1,12 @@
+<?php
+	//print_r($list);
+	/*foreach ($list as $k => $v) {
+		echo $k.'<br>';
+		$user_item = (object) $v;
+		echo $user_item->username .'<br>';
+	}
+	var_dump($list);*/
+	foreach ($list as $k => $v) {
+		echo $k .' : ' . $v . '<br />';
+	}
+ ?>
